a29e0a0d0d1cd0042010fa047ebdaa07fa8c5982
thewagesoffear <thewagesoffear@169376ae-9c2d-11de-b55d-05c6a1a974a6>
2010-11-02 13:57:41 +0000

    creating branch to build project on other,non-linux platforms

---
+++ branches/os-builds-tb/barchart-udt4/build/script/build-eclipse.sh
@@ -0,0 +1,187 @@
+#!/bin/sh
+
+# used by eclipse cdt interactive builder
+
+SCRIPT=`basename $0`
+log() {
+	echo "$SCRIPT: $1"
+}
+toLower() {
+  echo $1 | tr "[:upper:]" "[:lower:]" 
+} 
+toUpper() {
+  echo $1 | tr "[:lower:]" "[:upper:]" 
+} 
+
+KIND=$1
+
+LIB_NAME="SocketUDT"
+LIB_FOLDER="$PWD/../target/classes"
+
+ARTIFACT=""
+ARTIFACT_MAP=""
+
+LIB_FILE=""
+LIB_EXTENSION=""
+
+OS=`uname -s`
+OS=`toLower $OS`
+MACH=`uname -m`
+MACH=`toLower $MACH`
+
+makeExtension(){
+	case $OS in
+	linux)
+		LIB_EXTENSION="so"
+	;;
+	cygwin* | mingw* )
+		LIB_EXTENSION="dll"
+	;;
+	darwin )
+		LIB_EXTENSION="jnilib"
+	;;
+	*)
+		log "error: unsupported OS=$OS"
+		exit 1
+	;;
+	esac
+	log "detected LIB_EXTENSION=$LIB_EXTENSION"
+}
+findArtifact(){
+	COUNT=`ls -1 *.$LIB_EXTENSION | wc -l`
+	if [ $COUNT = 1 ] ; then
+		ARTIFACT=`ls -1 *.$LIB_EXTENSION`
+	else
+		log "error; can not find artifact; COUNT=$COUNT"
+		exit 1
+	fi 		
+}
+findArtifactMap(){
+	COUNT=`ls -1 *.map | wc -l`
+	if [ $COUNT = 1 ] ; then
+		ARTIFACT_MAP=`ls -1 *.map`
+	else
+		log "error; can not find artifact map; COUNT=$COUNT"
+		exit 1
+	fi 		
+}
+makeLibraryName() {
+	case $OS in
+			linux)
+			case $MACH in
+				i*86)
+					LIB_FILE="lib$LIB_NAME-linux-x86-32.so"
+				;;
+				amd64 | x86_64)
+					LIB_FILE="lib$LIB_NAME-linux-x86-64.so"
+				;;
+				*)
+					log "error; not supported MACH=$MACH"
+					exit 1
+				;;
+			esac		
+		;;
+		cygwin* | mingw* )
+			case $MACH in
+				i*86)
+					LIB_FILE="$LIB_NAME-windows-x86-32.dll"
+				;;
+				amd64 | x86_64)
+					LIB_FILE="$LIB_NAME-windows-x86-64.dll"
+				;;
+				*)
+					log "error; not supported MACH=$MACH"
+					exit 1
+				;;
+			esac		
+		;;
+		darwin )
+			case $MACH in
+				i*86)
+					LIB_FILE="lib$LIB_NAME-macosx-x86-64.jnilib"
+				;;
+				amd64 | x86_64)
+					LIB_FILE="lib$LIB_NAME-macosx-x86-64.jnilib"
+				;;
+				*)
+					log "error; not supported MACH=$MACH"
+					exit 1
+				;;
+			esac		
+		;;
+		*)
+			log "error: not supported OS=$OS"
+			exit 1
+		;;
+	esac
+}
+checkSupported(){
+	case $OS in
+		linux | cygwin* | mingw* | darwin )
+			log "detected OS=$OS"
+		;;
+		*)
+			log "error: unsupported OS=$OS"
+			exit 1
+		;;
+	esac
+	case $MACH in
+		i*86 | amd64 | x86_64 )
+			log "detected MACH=$MACH"
+		;;
+		*)
+			log "error: unsupported MACH=$MACH"
+			exit 1
+		;;
+	esac
+}
+
+#####################################################
+
+checkSupported
+makeExtension
+
+case $KIND in
+	start)
+		log "pre-build task;"
+		log "done"
+	;;
+	finish)
+		#
+		log "post-build task;"
+		findArtifact
+		findArtifactMap
+		makeLibraryName
+		#
+		MAP_FILE="$LIB_FILE.map"
+		SYM_FILE="$LIB_FILE.sym"
+		#
+		TARGET="$LIB_FOLDER/$LIB_FILE"
+		TARGET_MAP="$LIB_FOLDER/$MAP_FILE"
+		TARGET_SYM="$LIB_FOLDER/$SYM_FILE"
+		#
+		log "ARTIFACT     : $ARTIFACT"
+		log "ARTIFACT_MAP : $ARTIFACT_MAP"
+		log "TARGET     : $TARGET"
+		log "TARGET_MAP : $TARGET_MAP"
+		log "TARGET_SYM : $TARGET_SYM"
+		#
+		cp -f -v "$ARTIFACT" "$TARGET" 
+		cp -f -v "$ARTIFACT_MAP" "$TARGET_MAP"
+		
+		# TODO: find macosx alternatives
+		# nm --demangle --numeric-sort "$ARTIFACT" > "$TARGET_SYM" 
+		# ldd -r "$TARGET"
+		#
+		ls -l "$LIB_FOLDER"
+		log "done"
+	;;
+	*)
+		log "error; unecpected KIND=$KIND"
+		exit 1
+	;;
+esac
+
+exit 0
+
+#####################################################
---
+++ branches/os-builds-tb/test-deps/src/main/java/com/barchart/udt/test/MainLoadSocketUDT.java
@@ -0,0 +1,31 @@
+package com.barchart.udt.test;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.barchart.udt.SocketUDT;
+import com.barchart.udt.TypeUDT;
+
+public class MainLoadSocketUDT {
+
+	static final Logger log = LoggerFactory.getLogger(MainLoadSocketUDT.class);
+
+	public static void main(String[] args) {
+
+		log.info("this example tests if barchart-udt dependency works");
+
+		try {
+
+			SocketUDT socket = new SocketUDT(TypeUDT.DATAGRAM);
+
+			log.info("made socketID={}", socket.socketID);
+
+		} catch (Throwable e) {
+
+			log.error("can not make socket", e);
+
+		}
+
+	}
+
+}
