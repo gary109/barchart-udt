6826ed5c405fef3be27849e0cb78b8e4f085d661
Andrei.Pozolotin <Andrei.Pozolotin@169376ae-9c2d-11de-b55d-05c6a1a974a6>
2011-01-18 04:49:54 +0000

    [maven-release-plugin]  copy for tag barchart-udt4-parent-1.0.2

---
+++ tags/barchart-udt4-parent-1.0.2/barchart-udt4-bundle/src/main/java/com/barchart/udt/none/Empty.java
@@ -0,0 +1,7 @@
+package com.barchart.udt.none;
+
+public class Empty {
+
+	/** placeholder to bypass sonatype oss release rule : "must have javadoc" */
+
+}
---
+++ tags/barchart-udt4-parent-1.0.2/barchart-udt4/build/hudson/slave-props.sh
@@ -0,0 +1,9 @@
+#/bin/sh
+
+# single master
+MASTER="https://moe.barchart.com/hudson"
+
+# per-slave entry; enable what fits
+SLAVE="linux"
+#SLAVE="macosx"
+#SLAVE="windows"
