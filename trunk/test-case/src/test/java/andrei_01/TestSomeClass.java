5946c7085c924e3ac59f102ba83a5d961be5d0c2
Andrei.Pozolotin <Andrei.Pozolotin@169376ae-9c2d-11de-b55d-05c6a1a974a6>
2011-01-22 01:44:44 +0000

    project setup

---
+++ trunk/test-case/src/test/java/andrei_01/TestSomeClass.java
@@ -0,0 +1,26 @@
+package andrei_01;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestSomeClass {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testSomeFuction() {
+
+		assertTrue(true);
+
+	}
+
+}
