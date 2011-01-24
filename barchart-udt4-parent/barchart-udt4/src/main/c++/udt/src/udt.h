79f1a18546b16e85825f5600c019c198a9c17e08
Andrei.Pozolotin <Andrei.Pozolotin@169376ae-9c2d-11de-b55d-05c6a1a974a6>
2011-01-24 15:48:13 +0000

    update to udt trunk

--- barchart-udt4-parent/barchart-udt4/src/main/c++/udt/src/udt.h
+++ barchart-udt4-parent/barchart-udt4/src/main/c++/udt/src/udt.h
@@ -1,5 +1,5 @@
 /*****************************************************************************
-Copyright (c) 2001 - 2010, The Board of Trustees of the University of Illinois.
+Copyright (c) 2001 - 2011, The Board of Trustees of the University of Illinois.
 All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
@@ -35,7 +35,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 /*****************************************************************************
 written by
-   Yunhong Gu, last updated 12/31/2010
+   Yunhong Gu, last updated 01/18/2011
 *****************************************************************************/
 
 #ifndef __UDT_H__
@@ -48,7 +48,7 @@ written by
    #include <netinet/in.h>
 #else
    #ifdef __MINGW__
-      #include <stdint.h>
+      #include <sdtint.h>
       #include <ws2tcpip.h>
    #endif
    #include <windows.h>
