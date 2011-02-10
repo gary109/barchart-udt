b84468db1c571527b5b7371d2f7bc7539e51af93
Andrei.Pozolotin <Andrei.Pozolotin@169376ae-9c2d-11de-b55d-05c6a1a974a6>
2011-02-10 19:18:34 +0000

    try workaround for google code svn commit hang

--- barchart-udt4-parent/barchart-udt4/build/hudson/svn-site-deploy.sh
+++ barchart-udt4-parent/barchart-udt4/build/hudson/svn-site-deploy.sh
@@ -3,18 +3,30 @@
 # must provide project name
 PROJ="barchart-udt4"
 
-# google code convention
+# google code site convention
 BASE="$WORKSPACE/$PROJ"
 SITE="$WORKSPACE/site/$PROJ"
 
 echo "### BASE=$BASE"
 echo "### SITE=$SITE"
 
+### REMOVE
+
+echo "### svn cleanup/update"
+svn cleanup
+svn update
+
 echo "### svn delete"
 svn delete --force "$SITE"
 
 echo "### svn commit"
 svn commit --message "hudson: remove site" "$SITE"
+
+### PUBLISH
+
+echo "### svn cleanup/update"
+svn cleanup
+svn update
 
 echo "### mkdir new"
 mkdir --parents "$SITE"
@@ -31,6 +43,9 @@ find "$SITE" -name '*.html' -exec svn propset --force svn:mime-type text/html {}
 echo "### svn propset css"
 find "$SITE" -name '*.css'  -exec svn propset --force svn:mime-type text/css {} \;
 
-echo "### svn commit"
-svn commit --message "hudson: pulish site" "$SITE"
+echo "### svn cleanup/update"
+svn cleanup
+svn update
 
+echo "### svn commit"
+svn commit --message "hudson: publish site" "$SITE"
